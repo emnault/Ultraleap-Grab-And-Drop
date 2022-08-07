04ac5e2201c87e63e4842a328c06df7cf78e6af5
nault22e <en27@hw.ac.uk>
2022-08-07 14:05:58 +0100

    dual project working take 1 - all files

---
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using SensationTrackedDial;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public float X { get; set; }
+        
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            X = Example.appX;
+            Console.WriteLine("======" + X);
+        }
+    }
+}
---
+++ WebApplication1/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+const eventSource = new window.EventSource('/')
+
+eventSource.onmessage = (message) => {
+    if (!message || !message.data) return console.error('skipping empty message')
+    const live = JSON.parse(message.data, {})
+    console.log('sse live visitors', live)
+    // in a react component you could update the state
+    this.setState({ live })
+}
